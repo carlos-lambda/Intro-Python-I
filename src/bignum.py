01d950ec51e7e5baa49036f99b651e0eef8b1349
Carlos Lantigua <clantigua954@gmail.com>
2019-02-03 15:03:23 -0500

    completed bignum.py

--- src/bignum.py
+++ src/bignum.py
@@ -1,4 +1,4 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
-
+print(int(2)**int(65536))  # the int is not needed, I just like it
 # YOUR CODE HERE
